d48ff785897b6eced852cdd798b22910d8f6ed9d
Latrisha <latrisha.cheung@gmail.com>
2023-12-20 21:42:41 +0700

    update migration and seeder

--- database/seeders/BookingSlotSeeder.php
+++ database/seeders/BookingSlotSeeder.php
@@ -20,6 +20,7 @@ class BookingSlotSeeder extends Seeder
                 'date' => '2023-11-01',
                 'time_start' => '10:00:00',
                 'time_end' => '11:00:00',
+                'is_available' => false,
                 'created_at' => now(),
                 'updated_at' => now()
             ],
@@ -29,6 +30,7 @@ class BookingSlotSeeder extends Seeder
                 'date' => '2023-11-07',
                 'time_start' => '11:00:00',
                 'time_end' => '12:00:00',
+                'is_available' => true,
                 'created_at' => now(),
                 'updated_at' => now()
             ],
@@ -38,6 +40,7 @@ class BookingSlotSeeder extends Seeder
                 'date' => '2023-11-07',
                 'time_start' => '13:00:00',
                 'time_end' => '14:00:00',
+                'is_available' => false,
                 'created_at' => now(),
                 'updated_at' => now()
             ]
